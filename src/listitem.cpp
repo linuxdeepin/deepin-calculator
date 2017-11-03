8abc7df8c88060d7c459a50a29c15829356e2b9c
rekols <rekols@foxmail.com>
2017-11-03 21:55:47 +0800

    style: optimize qss

--- src/listitem.cpp
+++ src/listitem.cpp
@@ -1,4 +1,6 @@
 #include "listitem.h"
+#include <QFontMetrics>
+#include <QDebug>
 
 ListItem::ListItem()
 {
@@ -21,23 +23,24 @@ void ListItem::drawBackground(const QRect &rect, QPainter *painter)
 void ListItem::drawContent(const QRect &rect, QPainter *painter, bool isLast)
 {
     QFont font;
+    painter->setPen(QColor("#000000"));
 
-    if (isLast) {
-        painter->setPen(QColor("#3A3A3A"));
-        if (expression.length() < 15)
-            font.setPointSize(20);
-        else if (expression.length() >= 15 && expression.length() < 20)
-            font.setPointSize(17);
-        else if (expression.length() >= 20 && expression.length() < 25)
-            font.setPointSize(15);
-        else if (expression.length() >= 25 && expression.length() < 30)
-            font.setPointSize(13);
-        else
-            font.setPointSize(11);
-    } else {
-        painter->setPen(QColor("#636363"));
-        font.setPointSize(12);
-    }
+    // if (isLast) {
+    //     painter->setPen(QColor("#3A3A3A"));
+    //     if (expression.length() < 15)
+    //         font.setPointSize(20);
+    //     else if (expression.length() >= 15 && expression.length() < 20)
+    //         font.setPointSize(17);
+    //     else if (expression.length() >= 20 && expression.length() < 25)
+    //         font.setPointSize(15);
+    //     else if (expression.length() >= 25 && expression.length() < 30)
+    //         font.setPointSize(13);
+    //     else
+    //         font.setPointSize(11);
+    // } else {
+    //     painter->setPen(QColor("#636363"));
+    //     font.setPointSize(12);
+    // }
 
     painter->setFont(font);
     painter->drawText(rect, Qt::AlignVCenter | Qt::AlignRight, expression);
