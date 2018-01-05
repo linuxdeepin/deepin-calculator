c50e9446fe6bc415d742c9200878d0476a2042a5
transifex <transifex@linuxdeepin.com>
2018-01-05 05:54:29 +0000

    auto sync po files from transifex
    Change-Id: I90cd48b8ad8f91947ee26491063c9db771eb97d8

---
+++ translations/deepin-calculator_nl.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="nl" version="2.1">
+<context>
+    <name>ExpressionList</name>
+    <message>
+        <source>Expression Error</source>
+        <translation>Uitdrukking fout</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Deepin Calculator</source>
+        <translation>Deepin Rekenmachine</translation>
+    </message>
+    <message>
+        <source>Deepin Calculator is a simple and easy to use desktop calculator. It supports addition, subtraction, multiplication and division.</source>
+        <translation>Deepin Rekenmachine is een eenvoudige en gebruiksvriendelijke desktop rekenmachine. Het ondersteunt optellen, aftrekken, vermenigvuldigen en delen.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Deepin Calculator</source>
+        <translation>Deepin Rekenmachine</translation>
+    </message>
+    <message>
+        <source>Dark Theme</source>
+        <translation>Donker Thema</translation>
+    </message>
+</context>
+</TS>
