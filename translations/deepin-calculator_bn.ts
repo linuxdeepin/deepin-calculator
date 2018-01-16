c9bfafc150a57289f10e6e956c3bb928f3ff354d
transifex <transifex@linuxdeepin.com>
2018-01-16 17:03:01 +0000

    auto sync po files from transifex
    Change-Id: Ie9446b4a341370479accfe6a44a8698d40d00183

---
+++ translations/deepin-calculator_bn.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="bn" version="2.1">
+<context>
+    <name>ExpressionList</name>
+    <message>
+        <source>Expression Error</source>
+        <translation>ত্রুটিপূর্ণ সমীকরণ</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Deepin Calculator</source>
+        <translation>ডিপিন ক্যালকুলেটর</translation>
+    </message>
+    <message>
+        <source>Deepin Calculator is a simple and easy to use desktop calculator. It supports addition, subtraction, multiplication and division.</source>
+        <translation>ডিপিন ক্যালকুলেটর একটি সাধারণ সহজে ব্যবহার যোগ্য ডেস্কটপ ক্যালকুলেটর। এটা যোগ, বিয়োগ, গুণ এবং ভাগ করতে পারে।</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Deepin Calculator</source>
+        <translation>ডিপিন ক্যালকুলেটর</translation>
+    </message>
+    <message>
+        <source>Dark Theme</source>
+        <translation>কালো থীম</translation>
+    </message>
+</context>
+</TS>
--- translations/deepin-calculator_gl_ES.ts
+++ translations/deepin-calculator_gl_ES.ts
@@ -1,31 +1,31 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="ar" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="gl_ES" version="2.1">
 <context>
     <name>ExpressionList</name>
     <message>
         <source>Expression Error</source>
-        <translation>خطأ في المصطح</translation>
+        <translation>Erro na expresión</translation>
     </message>
 </context>
 <context>
     <name>Main</name>
     <message>
         <source>Deepin Calculator</source>
-        <translation>حاسبة ديبين</translation>
+        <translation>Calculadora</translation>
     </message>
     <message>
         <source>Deepin Calculator is a simple and easy to use desktop calculator. It supports addition, subtraction, multiplication and division.</source>
-        <translation>حاسبة ديبين : إنها بسيطة وسهلة لاستخدمات سطح المكتب ، تدعم عمليات الجمع والطرح والضرب والقسمة.</translation>
+        <translation>Calculadora é unha ferramenta simple e sinxela para realizar operacións de escritorio. Pode realizar sumas, restas, multiplicacións e divisións.</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
         <source>Deepin Calculator</source>
-        <translation>حاسبة ديبين</translation>
+        <translation>Calculadora</translation>
     </message>
     <message>
         <source>Dark Theme</source>
-        <translation>سمة داكنة</translation>
+        <translation>Tema Escuro</translation>
     </message>
 </context>
 </TS>
