807d6c6e01a8dcd363114cb6217b4f0a434b5686
pengfei xie <pengfei.xie@deepin.io>
2019-11-29 14:54:31 +0800

    fix:Revision of copywriting
    Revision of copywriting

--- translations/deepin-calculator.ts
+++ translations/deepin-calculator.ts
@@ -1669,7 +1669,7 @@
     </message>
     <message>
         <source>All election</source>
-        <translation>选择全部</translation>
+        <translation type="vanished">选择全部</translation>
     </message>
     <message>
         <source>Delete</source>
@@ -1722,6 +1722,10 @@
     <message>
         <source>Clear</source>
         <translation>清除</translation>
+    </message>
+    <message>
+        <source>select all</source>
+        <translation>选择全部</translation>
     </message>
 </context>
 <context>
