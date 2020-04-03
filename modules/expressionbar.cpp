99387920e5634360cf9f376a48d572ed283e38aa
jing.xia <jing.xia@archermind.com>
2020-04-03 09:43:36 +0800

    Type:fix
    Descripton:When the expression is wrong, input the number and equal then
    re-enter a new expression, the error expression in the history record
    has not been replaced.
    An expression error reenters a new expression with no result in the input edit.
    
    RootCause:Enter the wrong judgment.
    
    Solution:Reset the judgement.

--- modules/expressionbar.cpp
+++ modules/expressionbar.cpp
@@ -586,6 +586,7 @@ void ExpressionBar::enterEqualEvent()
     m_hisRevision = -1;
     m_listView->scrollToBottom();
     m_isResult = false;*/
+    QString exp = m_inputEdit->text();
     if (m_inputEdit->text().isEmpty())
         return;
     if (!m_isLinked)
@@ -601,7 +602,8 @@ void ExpressionBar::enterEqualEvent()
     }
     Quantity ans = m_evaluator->evalUpdateAns();
     QString newResult;
-    if (m_evaluator->error().isEmpty()) {
+    // 20200403 bug-18971 表达式错误时输数字加等于再重新输入表达式历史记录错误表达式未被替换
+    if (m_evaluator->error().isEmpty() && (exp.indexOf(QRegExp("[＋－×÷,%()e]")) != -1)) {
         if (ans.isNan() && !m_evaluator->isUserFunctionAssign())
             return;
         QString formatResult;
@@ -628,7 +630,11 @@ void ExpressionBar::enterEqualEvent()
         }
         m_isContinue = false;
     } else {
-        m_listModel->updataList(m_inputEdit->text() + "＝" + tr("Expression error"), m_hisRevision);
+        // 20200403 bug-18971 表达式错误时输数字加等于再重新输入表达式历史记录错误表达式未被替换
+        if (!m_evaluator->error().isEmpty()) {
+            m_listModel->updataList(m_inputEdit->text() + "＝" + tr("Expression error"),
+                                    m_hisRevision);
+        }
         if (m_hisRevision == -1)
             m_hisRevision = m_listModel->rowCount(QModelIndex()) - 1;
         else {
@@ -678,8 +684,9 @@ void ExpressionBar::enterEqualEvent()
                 enterEqualEvent();
             }
         }
-        if (m_evaluator->error().isEmpty())
-            m_inputEdit->clear();
+        // 20200403 表达式错误重新输入新表达式输入框无结果
+        //        if (m_evaluator->error().isEmpty())
+        //            m_inputEdit->clear();
     }
 
     if (m_evaluator->error().isEmpty())
