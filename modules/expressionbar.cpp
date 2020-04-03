01e486f38eac7e58076486ed3dc6b4cfc4868bdf
jing.xia <jing.xia@archermind.com>
2020-04-03 10:21:19 +0800

    Type:fix
    Descripton:When the expression is wrong, input the number and equal then
    re-enter a new expression, the error expression in the history record
    has not been replaced.
    An expression error reenters a new expression with no result in the input edit.
    
    RootCause:Enter the wrong judgment.
    
    Solution:Reset the judgement.

--- modules/expressionbar.cpp
+++ modules/expressionbar.cpp
@@ -688,8 +688,8 @@ void ExpressionBar::enterEqualEvent()
         //        if (m_evaluator->error().isEmpty())
         //            m_inputEdit->clear();
     }
-
-    if (m_evaluator->error().isEmpty())
+    // 20200403 bug-18971 表达式错误时输数字加等于再重新输入表达式历史记录错误表达式未被替换
+    if (m_evaluator->error().isEmpty() && (exp.indexOf(QRegExp("[＋－×÷,.%()e]")) != -1))
         m_hisRevision = -1;
     m_listView->scrollToBottom();
     m_isLinked = false;
