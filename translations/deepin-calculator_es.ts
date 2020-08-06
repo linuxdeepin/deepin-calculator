da636bc7a8285aceba70034cebe82e889a329770
jingzhou <jingzhou@uniontech.com>
2020-08-06 13:54:12 +0800

    chore:增加翻译
    Description: 增加新的翻译文件
    
    Log: 增加翻译
    
    link:

---
+++ translations/deepin-calculator_es.ts
@@ -0,0 +1,193 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<context>
+    <name>ExpressionBar</name>
+    <message>
+        <source>Expression error</source>
+        <translation>Error de expresión</translation>
+    </message>
+</context>
+<context>
+    <name>IconButton</name>
+    <message>
+        <source>Clear all memory</source>
+        <translation>Borrar toda la memoria</translation>
+    </message>
+    <message>
+        <source>Clear history</source>
+        <translation>Limpia historial</translation>
+    </message>
+</context>
+<context>
+    <name>InputEdit</name>
+    <message>
+        <source>&amp;Undo</source>
+        <translation>Deshacer (&amp;U)</translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>Rehacer (&amp;R)</translation>
+    </message>
+    <message>
+        <source>Cu&amp;t</source>
+        <translation>Cortar (&amp;T)</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>Copiar (&amp;C)</translation>
+    </message>
+    <message>
+        <source>&amp;Paste</source>
+        <translation>Pegar (&amp;P)</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <source>Select All</source>
+        <translation>Seleccionar todo</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Calculator</source>
+        <translation>Calculadora</translation>
+    </message>
+    <message>
+        <source>Calculator is an easy to use desktop calculator, supporting standard and scientific modes.</source>
+        <translation>Es una calculadora de escritorio fácil de usar, que soporta los modos estándar y científico.</translation>
+    </message>
+    <message>
+        <source>Standard</source>
+        <translation>Estándar</translation>
+    </message>
+    <message>
+        <source>Scientific</source>
+        <translation>Científica</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>Historial</translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>Modo</translation>
+    </message>
+</context>
+<context>
+    <name>MemoryButton</name>
+    <message>
+        <source>Clear memory item</source>
+        <translation>Borrar el elemento de memoria</translation>
+    </message>
+    <message>
+        <source>Clear all memory</source>
+        <translation>Borrar toda la memoria</translation>
+    </message>
+    <message>
+        <source>Memory recall</source>
+        <translation>Recuperar memoria</translation>
+    </message>
+    <message>
+        <source>Add to memory item</source>
+        <translation>Sumar elemento a la memoria</translation>
+    </message>
+    <message>
+        <source>Memory add</source>
+        <translation>Sumar a la memoria </translation>
+    </message>
+    <message>
+        <source>Subtract from memory item</source>
+        <translation>Restar elemento a la memoria</translation>
+    </message>
+    <message>
+        <source>Memory subtract</source>
+        <translation>Restar a la memoria</translation>
+    </message>
+    <message>
+        <source>Memory store</source>
+        <translation>Guardar en memoria</translation>
+    </message>
+    <message>
+        <source>Memory</source>
+        <translation>Memoria</translation>
+    </message>
+</context>
+<context>
+    <name>MemoryItemWidget</name>
+    <message>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <source>Clear memory item</source>
+        <translation>Borrar el elemento de la memoria</translation>
+    </message>
+    <message>
+        <source>Add to memory item</source>
+        <translation>Sumar elemento a la memoria</translation>
+    </message>
+    <message>
+        <source>Subtract from memory item</source>
+        <translation>Restar elemento a la memoria</translation>
+    </message>
+</context>
+<context>
+    <name>MemoryWidget</name>
+    <message>
+        <source>Nothing saved in memory</source>
+        <translation>No hay nada guardado en la memoria</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Calculator</source>
+        <translation>Calculadora</translation>
+    </message>
+</context>
+<context>
+    <name>SciExpressionBar</name>
+    <message>
+        <source>Expression error</source>
+        <translation>Error de expresión</translation>
+    </message>
+</context>
+<context>
+    <name>SciHistoryWidget</name>
+    <message>
+        <source>History</source>
+        <translation>Historial</translation>
+    </message>
+    <message>
+        <source>Memory</source>
+        <translation>Memoria</translation>
+    </message>
+</context>
+<context>
+    <name>SimpleListDelegate</name>
+    <message>
+        <source>Expression error</source>
+        <translation>Error de expresión</translation>
+    </message>
+</context>
+<context>
+    <name>SimpleListModel</name>
+    <message>
+        <source>No history</source>
+        <translation>No hay historial</translation>
+    </message>
+</context>
+<context>
+    <name>SimpleListView</name>
+    <message>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+</context>
+</TS>
