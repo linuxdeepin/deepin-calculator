556275ddc17be4f1bdbedc564ce11646b8b94d73
jingzhou <jingzhou@uniontech.com>
2020-09-03 15:34:41 +0800

    chore:补充翻译
    继续新增翻译
    
    Log: 补充翻译
    
    link:

---
+++ translations/deepin-calculator_hu.ts
@@ -0,0 +1,193 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="hu" version="2.1">
+<context>
+    <name>ExpressionBar</name>
+    <message>
+        <source>Expression error</source>
+        <translation>Kifejezéshiba</translation>
+    </message>
+</context>
+<context>
+    <name>IconButton</name>
+    <message>
+        <source>Clear all memory</source>
+        <translation>Memória teljes törlése</translation>
+    </message>
+    <message>
+        <source>Clear history</source>
+        <translation>Előzmény törlése</translation>
+    </message>
+</context>
+<context>
+    <name>InputEdit</name>
+    <message>
+        <source>&amp;Undo</source>
+        <translation>Visszavonás (&amp;U)</translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>Újra (&amp;R)</translation>
+    </message>
+    <message>
+        <source>Cu&amp;t</source>
+        <translation>Kivágás (&amp;T)</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>Másolás (&amp;C)</translation>
+    </message>
+    <message>
+        <source>&amp;Paste</source>
+        <translation>Beillesztés (&amp;P)</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Törlés</translation>
+    </message>
+    <message>
+        <source>Select All</source>
+        <translation>Összes kijelölése</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Calculator</source>
+        <translation>Számológép</translation>
+    </message>
+    <message>
+        <source>Calculator is an easy to use desktop calculator, supporting standard and scientific modes.</source>
+        <translation>A Számológép egy könnyen használható asztali számológép, amely támogatja a szabványos és tudományos módokat.</translation>
+    </message>
+    <message>
+        <source>Standard</source>
+        <translation>Hagyományos</translation>
+    </message>
+    <message>
+        <source>Scientific</source>
+        <translation>Tudományos</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>Előzmény</translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>Mód</translation>
+    </message>
+</context>
+<context>
+    <name>MemoryButton</name>
+    <message>
+        <source>Clear memory item</source>
+        <translation>Elem törlése a memóriából</translation>
+    </message>
+    <message>
+        <source>Clear all memory</source>
+        <translation>Memória teljes törlése</translation>
+    </message>
+    <message>
+        <source>Memory recall</source>
+        <translation>Memória előhívása</translation>
+    </message>
+    <message>
+        <source>Add to memory item</source>
+        <translation>Elem hozzáadása a memóriához</translation>
+    </message>
+    <message>
+        <source>Memory add</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Subtract from memory item</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Memory subtract</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Memory store</source>
+        <translation>Tárolás a memóriában</translation>
+    </message>
+    <message>
+        <source>Memory</source>
+        <translation>Memória</translation>
+    </message>
+</context>
+<context>
+    <name>MemoryItemWidget</name>
+    <message>
+        <source>Copy</source>
+        <translation>Másolás</translation>
+    </message>
+    <message>
+        <source>Clear memory item</source>
+        <translation>Elem törlése a memóriából</translation>
+    </message>
+    <message>
+        <source>Add to memory item</source>
+        <translation>Elem hozzáadása a memóriához</translation>
+    </message>
+    <message>
+        <source>Subtract from memory item</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>MemoryWidget</name>
+    <message>
+        <source>Nothing saved in memory</source>
+        <translation>Üres a memória</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Calculator</source>
+        <translation>Számológép</translation>
+    </message>
+</context>
+<context>
+    <name>SciExpressionBar</name>
+    <message>
+        <source>Expression error</source>
+        <translation>Kifejezéshiba</translation>
+    </message>
+</context>
+<context>
+    <name>SciHistoryWidget</name>
+    <message>
+        <source>History</source>
+        <translation>Előzmény</translation>
+    </message>
+    <message>
+        <source>Memory</source>
+        <translation>Memória</translation>
+    </message>
+</context>
+<context>
+    <name>SimpleListDelegate</name>
+    <message>
+        <source>Expression error</source>
+        <translation>Kifejezéshiba</translation>
+    </message>
+</context>
+<context>
+    <name>SimpleListModel</name>
+    <message>
+        <source>No history</source>
+        <translation>Nincs előzmény</translation>
+    </message>
+</context>
+<context>
+    <name>SimpleListView</name>
+    <message>
+        <source>Copy</source>
+        <translation>Másolás</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Törlés</translation>
+    </message>
+</context>
+</TS>
