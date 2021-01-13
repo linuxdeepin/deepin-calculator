228fe6ecc24975cae8eccaab8893140af39bce4b
jingzhou <jingzhou@uniontech.com>
2021-01-13 16:47:22 +0800

    chore: 补充翻译
    补充新增的翻译
    
    Log: 补充翻译
    Change-Id: I2e3e8e4507c0c601c364cb8c448605156d7a569a

---
+++ translations/deepin-calculator_ms.ts
@@ -0,0 +1,245 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ms" version="2.1">
+<context>
+    <name>ExpressionBar</name>
+    <message>
+        <source>Expression error</source>
+        <translation>Ralat ungkapan</translation>
+    </message>
+</context>
+<context>
+    <name>IconButton</name>
+    <message>
+        <source>Clear all memory</source>
+        <translation>Kosongkan semua ingatan</translation>
+    </message>
+    <message>
+        <source>Clear history</source>
+        <translation>Kosongkan sejarah</translation>
+    </message>
+</context>
+<context>
+    <name>InputEdit</name>
+    <message>
+        <source>Delete</source>
+        <translation>Padam</translation>
+    </message>
+    <message>
+        <source>Select All</source>
+        <translation>Pilih Semua</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Buat asal</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Buat semula</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Potong</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Salin</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Tampal</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Calculator</source>
+        <translation>Kalkulator</translation>
+    </message>
+    <message>
+        <source>Standard</source>
+        <translation>Piawai</translation>
+    </message>
+    <message>
+        <source>Scientific</source>
+        <translation>Saintifik</translation>
+    </message>
+    <message>
+        <source>Mode</source>
+        <translation>Mod</translation>
+    </message>
+    <message>
+        <source>Calculator is an easy to use desktop calculator, supporting standard, scientific and programmer modes.</source>
+        <translation>Kalkulator ialah sebuah kalkulator atas meja yang ringkas dan mudah digunakan. Ia menyokong mod-mod piawai, saintifik dan pengatur cara</translation>
+    </message>
+    <message>
+        <source>Programmer</source>
+        <translation>Pengatur cara</translation>
+    </message>
+</context>
+<context>
+    <name>MemoryButton</name>
+    <message>
+        <source>Clear memory item</source>
+        <translation>Kosongkan item ingatan</translation>
+    </message>
+    <message>
+        <source>Clear all memory</source>
+        <translation>Kosongkan semua ingatan</translation>
+    </message>
+    <message>
+        <source>Memory recall</source>
+        <translation>Ingat semula ingatan</translation>
+    </message>
+    <message>
+        <source>Add to memory item</source>
+        <translation>Tambah ke item ingatan</translation>
+    </message>
+    <message>
+        <source>Memory add</source>
+        <translation>Tambah ingatan</translation>
+    </message>
+    <message>
+        <source>Subtract from memory item</source>
+        <translation>Buang daripada item ingatan</translation>
+    </message>
+    <message>
+        <source>Memory subtract</source>
+        <translation>Buang ingatan</translation>
+    </message>
+    <message>
+        <source>Memory store</source>
+        <translation>Simpanan ingatan</translation>
+    </message>
+    <message>
+        <source>Memory</source>
+        <translation>Ingatan</translation>
+    </message>
+</context>
+<context>
+    <name>MemoryItemWidget</name>
+    <message>
+        <source>Copy</source>
+        <translation>Salin</translation>
+    </message>
+    <message>
+        <source>Clear memory item</source>
+        <translation>Kosongkan item ingatan</translation>
+    </message>
+    <message>
+        <source>Add to memory item</source>
+        <translation>Tambah ke item ingatan</translation>
+    </message>
+    <message>
+        <source>Subtract from memory item</source>
+        <translation>Buang daripada item ingatan</translation>
+    </message>
+</context>
+<context>
+    <name>MemoryWidget</name>
+    <message>
+        <source>Nothing saved in memory</source>
+        <translation>Tiada apa yang tersimpan dalam ingatan</translation>
+    </message>
+</context>
+<context>
+    <name>ProExpressionBar</name>
+    <message>
+        <source>Expression error</source>
+        <translation>Ralat ungkapan</translation>
+    </message>
+</context>
+<context>
+    <name>ProListView</name>
+    <message>
+        <source>Copy</source>
+        <translation>Salin</translation>
+    </message>
+</context>
+<context>
+    <name>ProgramModule</name>
+    <message>
+        <source>Arithmetic shift</source>
+        <translation>Anjak aritmatik</translation>
+    </message>
+    <message>
+        <source>Logical shift</source>
+        <translation>Anjak logikal</translation>
+    </message>
+    <message>
+        <source>Circular shift</source>
+        <translation>Anjak membulat</translation>
+    </message>
+    <message>
+        <source>Rotate through carry circular shift</source>
+        <translation>Putar melalui anjak membulat</translation>
+    </message>
+</context>
+<context>
+    <name>ProgrammerItemWidget</name>
+    <message>
+        <source>Arithmetic shift</source>
+        <translation>Anjak aritmatik</translation>
+    </message>
+    <message>
+        <source>Logical shift</source>
+        <translation>Anjak logikal</translation>
+    </message>
+    <message>
+        <source>Circular shift</source>
+        <translation>Anjak membulat</translation>
+    </message>
+    <message>
+        <source>Rotate through carry circular shift</source>
+        <translation>Putar melalui anjak membulat</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Calculator</source>
+        <translation>Kalkulator</translation>
+    </message>
+</context>
+<context>
+    <name>SciExpressionBar</name>
+    <message>
+        <source>Expression error</source>
+        <translation>Ralat ungkapan</translation>
+    </message>
+</context>
+<context>
+    <name>SciHistoryWidget</name>
+    <message>
+        <source>History</source>
+        <translation>Sejarah</translation>
+    </message>
+    <message>
+        <source>Memory</source>
+        <translation>Ingatan</translation>
+    </message>
+</context>
+<context>
+    <name>SimpleListDelegate</name>
+    <message>
+        <source>Expression error</source>
+        <translation>Ralat ungkapan</translation>
+    </message>
+</context>
+<context>
+    <name>SimpleListModel</name>
+    <message>
+        <source>No history</source>
+        <translation>Tiada sejarah</translation>
+    </message>
+</context>
+<context>
+    <name>SimpleListView</name>
+    <message>
+        <source>Copy</source>
+        <translation>Salin</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Padam</translation>
+    </message>
+</context>
+</TS>
