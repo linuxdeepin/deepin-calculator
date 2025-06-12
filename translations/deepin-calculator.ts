ff39a3c89c5efdfeceb4908bed155782a82ae6d7
Tian Shilin <tianshilin@uniontech.com>
2025-06-12 15:43:07 +0800

    fix: test CI

--- translations/deepin-calculator.ts
+++ translations/deepin-calculator.ts
@@ -216,6 +216,30 @@
         <source>Current theme type:</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Testing translations with longer sentences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This is a calculator application that supports basic arithmetic operations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Scientific mode provides advanced mathematical functions and constants</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Programmer mode offers binary, octal, decimal and hexadecimal calculations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>History view shows all previous calculations and their results</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Memory functions allow you to store and recall calculation results</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>SciExpressionBar</name>
